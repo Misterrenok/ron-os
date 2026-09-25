cd426dcccbb8a0efa03697f0ee33040fc0a257d3
Misterrenok <52697351+Misterrenok@users.noreply.github.com>
2026-09-25 16:31:28 -0400

    Guard current Bebris lesson 2 action link

--- system/lifeup/cloud/test/player-utility.test.mjs
+++ system/lifeup/cloud/test/player-utility.test.mjs
@@ -5,10 +5,11 @@ import { readFile } from 'node:fs/promises';
 const utility = await readFile(new URL('../public/player-utility.js', import.meta.url), 'utf8');
 const shell = await readFile(new URL('../public/index-v2.html', import.meta.url), 'utf8');
 
-test('current German A0 quest exposes the canonical Bebris lesson 1 execution action', () => {
-  assert.match(utility, /qv2-german-a0-bebris-lesson1-20260917/);
-  assert.match(utility, /https:\/\/www\.youtube\.com\/watch\?v=gd1anD8rYJY&list=PLpvrYdFr65lclosateOsp59ho-R34TrgJ/);
+test('current German A0 quest exposes the canonical Bebris lesson 2 execution action', () => {
+  assert.match(utility, /qv2-german-a0-bebris-lesson2-20260925/);
+  assert.match(utility, /https:\/\/germangalaxy\.mave\.digital\/ep-2/);
   assert.match(utility, /НАЧАТЬ УРОК БЕБРИСА/);
+  assert.doesNotMatch(utility, /qv2-german-a0-bebris-lesson1-20260917/);
   assert.doesNotMatch(utility, /qv2-german-a0-first-greetings-20260917/);
   assert.doesNotMatch(utility, /qv2-german-nicos-weg-a1-hallo-recovery-20260912/);
   assert.doesNotMatch(utility, /learngerman\.dw\.com\/ru\/hallo/);
